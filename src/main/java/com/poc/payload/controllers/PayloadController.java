f05cfaa3d2c50dfdd2d8c6a6303e03e195a1a767
DionatanLeao <dionatan.ti@outlook.com>
2022-05-13 12:20:34 -0300

    Disponibilizando crud endpoint /payloads

--- src/main/java/com/poc/payload/controllers/PayloadController.java
+++ src/main/java/com/poc/payload/controllers/PayloadController.java
@@ -1,12 +1,19 @@
 package com.poc.payload.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.poc.payload.entities.Payload;
 import com.poc.payload.services.PayloadService;
@@ -23,4 +30,30 @@ public class PayloadController {
 		List<Payload> payloads = service.findAll();
 		return ResponseEntity.ok().body(payloads);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Payload> findById(@PathVariable Long id) {
+		Payload payload = service.findById(id);
+		return ResponseEntity.ok().body(payload);		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Payload> save(@RequestBody Payload payload) {
+		Payload payloadSaved = service.save(payload);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(payload.getId()).toUri();
+		return ResponseEntity.created(uri).body(payloadSaved);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Payload> update(@RequestBody Payload payloadUpdate, @PathVariable Long id) {
+		Payload payload = service.update(payloadUpdate, id);
+		return ResponseEntity.ok().body(payload);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) { 
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/poc/payload/services/PayloadService.java
+++ src/main/java/com/poc/payload/services/PayloadService.java
@@ -2,6 +2,8 @@ package com.poc.payload.services;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +21,30 @@ public class PayloadService {
 	}
 	
 	public Payload findById(Long id) {
-		Payload payload = repo.findById(id).orElseThrow();
+		Payload payload = repo.findById(id).orElseThrow(() -> new EntityNotFoundException("Not found: " + id));
 		return payload;
 	}
-}
+	
+	public Payload save(Payload payload) {
+		return repo.save(payload);
+	}
+	
+	public Payload update(Payload payloadUpdate, Long id) {
+		Payload payload = findById(id);
+		Payload payloadUpdated = updateData(payload, payloadUpdate);
+		return repo.save(payloadUpdated);
+		
+	}
+	
+	private Payload updateData(Payload payload, Payload payloadUpdate) {
+		payload.setFormCode(payloadUpdate.getFormCode());
+		payload.setFileName(payloadUpdate.getFileName());
+		payload.setContent(payloadUpdate.getContent());
+		return payload;
+	}
+	
+	public void delete(Long id) {
+		Payload payload = findById(id);
+		repo.delete(payload);
+	}
+ }
