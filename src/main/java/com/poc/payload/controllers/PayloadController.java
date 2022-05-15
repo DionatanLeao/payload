299191e821cb9e779bed34ef19e873ccd182e5e5
DionatanLeao <dionatan.ti@outlook.com>
2022-05-15 12:24:29 -0300

    Adicionando configuração CrossOrigin

--- src/main/java/com/poc/payload/controllers/PayloadController.java
+++ src/main/java/com/poc/payload/controllers/PayloadController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -20,6 +22,7 @@ import com.poc.payload.services.PayloadService;
 
 @RestController
 @RequestMapping("/payloads")
+@CrossOrigin(origins = "http://localhost:8080", methods = RequestMethod.PUT)
 public class PayloadController {
 	
 	@Autowired
