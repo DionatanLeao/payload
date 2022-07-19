e6579384c6edbd1e4a40e5a8f43da3fc3adf90a5
DionatanLeao <dionatan.ti@outlook.com>
2022-07-19 13:10:33 -0300

    Ajustando testes payloadDtoDTOTest

--- src/test/java/com/poc/payload/domain/dto/PayloadDTOTest.java
+++ src/test/java/com/poc/payload/domain/dto/PayloadDTOTest.java
@@ -3,10 +3,14 @@ package com.poc.payload.domain.dto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class payloadDtoDTOTest {
 	
+	private static final String TYPE = "type";
+	private static final String FIELD_NAME = "fieldName";
 	private static final String TOKEN = "token";
 	private static final String CONTENT = "content";
 	private static final String FILE_NAME = "arquivo_0";
@@ -20,16 +24,18 @@ class payloadDtoDTOTest {
 		payloadDto.setId(ID);
 		payloadDto.setFormCode(FORM_CODE);
 		payloadDto.setFileName(FILE_NAME);
-		payloadDto.setContent(CONTENT);
 		payloadDto.setToken(TOKEN);
+		
+		FieldListDTO fieldListDto =  new FieldListDTO(FIELD_NAME, CONTENT, TYPE);
+		payloadDto.setFieldList(Arrays.asList(fieldListDto));
 
 		assertNotNull(payloadDto);
 
 		assertEquals(ID, payloadDto.getId());
 		assertEquals(FORM_CODE, payloadDto.getFormCode());
 		assertEquals(FILE_NAME, payloadDto.getFileName());
-		assertEquals(CONTENT, payloadDto.getContent());
 		assertEquals(TOKEN, payloadDto.getToken());
+		assertEquals(CONTENT, payloadDto.getFieldList());
 	}
 
 }
